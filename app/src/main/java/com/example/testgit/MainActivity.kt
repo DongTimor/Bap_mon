e35d6cf549ba25cd3ee6a958ad5ba289dcd8c610
DongTimor <105773677+DongTimor@users.noreply.github.com>
2023-09-29 20:47:43 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/testgit/MainActivity.kt
+++ app/src/main/java/com/example/testgit/MainActivity.kt
@@ -15,6 +15,7 @@ import com.example.testgit.ui.theme.TestGitTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var x = 0
         setContent {
             TestGitTheme {
                 // A surface container using the 'background' color from the theme
